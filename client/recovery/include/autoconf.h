4ff343ce9692cfa642f1ec6a048cfc70564299df
ZhangYanMing <yanming.zhang@ingenic.com>
2016-09-28 20:16:12 +0800

    update
    Change-Id: I17fbf2129b75b677655f602ada023b7361486829

---
+++ client/recovery/include/autoconf.h
@@ -0,0 +1,17 @@
+/*
+ *
+ * Automatically generated file; DO NOT EDIT.
+ *
+ */
+
+#define CONFIG_ROOTFS_TYPE "yaffs2"
+#define CONFIG_USERFS_TYPE "yaffs2"
+#define CONFIG_FLASH_CAPACITY 128
+#define CONFIG_NET_INTERFACE_NAME "eth0"
+#define CONFIG_SERVER_IP "194.169.2.59"
+#define CONFIG_SERVER_URL "http://194.169.2.59:8008"
+#define CONFIG_CONFIGURE_FILE_PATH "autoupdate/autoupdate.txt"
+#define CONFIG_STORAGE_MEDIUM_TYPE 1
+#define CONFIG_STORAGE_MEDIUM_MOUNT_POINT "/mnt/sdcard"
+#define CONFIG_ROOTFS_MOUNT_POINT "/mnt/rootfs"
+#define CONFIG_USERFS_MOUNT_POINT "/mnt/mtdblock"
