7ab65c599354a1051e08c086ad43578fd99b5722
ZhangYanMing <yanming.zhang@ingenic.com>
2016-10-11 16:38:10 +0800

    add libgpio test unit
    Change-Id: Ic5ff591a829d53c77469ca18e220a4fa57a92611

--- client/recovery/lib/gpio/libgpio.c
+++ client/recovery/lib/gpio/libgpio.c
@@ -58,6 +58,7 @@ static int export_un_export(struct gpio_pin* pin, uint32_t export) {
         goto close_out;
     }
 
+    len = write (fd, c, ret);
     if (len != ret) {
         ret = -errno;
         pin->valid = GPIO_INVALID;
@@ -185,16 +186,16 @@ close_out:
     return ret;
 }
 
-static int set_irq (struct gpio_pin *pin, gpio_irq_mode m) {
+static int set_irq(struct gpio_pin *pin, gpio_irq_mode m) {
     char c[STR_LEN];
     int fd, ret = 0;
 
-    ret = snprintf (c, STR_LEN, SYSFS"gpio%d/edge", pin->no);
+    ret = snprintf(c, STR_LEN, SYSFS"gpio%d/edge", pin->no);
 
     if (ret < 0)
         return ret;
 
-    fd = open (c, O_RDWR);
+    fd = open(c, O_RDWR);
     if (fd == -1) {
         ret = -errno;
         return ret;
@@ -202,25 +203,25 @@ static int set_irq (struct gpio_pin *pin, gpio_irq_mode m) {
 
     switch (m) {
         case GPIO_RISING:
-            ret = write (fd, "rising", 6);
+            ret = write(fd, "rising", 6);
             if (ret != 6)
                 ret = -1;
             break;
 
         case GPIO_FALLING:
-            ret = write (fd, "falling", 7);
+            ret = write(fd, "falling", 7);
             if (ret != 7)
                 ret = -1;
             break;
 
         case GPIO_BOTH:
-            ret = write (fd, "both", 4);
+            ret = write(fd, "both", 4);
             if (ret != 4)
                 ret = -1;
             break;
 
         case GPIO_NONE:
-            ret = write (fd, "none", 4);
+            ret = write(fd, "none", 4);
             if (ret != 4)
                 ret = -1;
             break;
@@ -243,10 +244,10 @@ close_out:
     return ret;
 }
 
-int gpio_out (struct gpio_pin *pin) {
+int gpio_out(struct gpio_pin *pin) {
     int ret = 0;
 
-    LOGD("%s: %d\n", __FUNCTION__, pin->no);
+    LOGD("%s: %d", __FUNCTION__, pin->no);
 
     ret = direction(pin, "out\0");
 
@@ -256,12 +257,12 @@ int gpio_out (struct gpio_pin *pin) {
     return ret;
 }
 
-int gpio_in (struct gpio_pin *pin) {
+int gpio_in(struct gpio_pin *pin) {
     int ret = 0;
 
-    LOGD("%s: %d\n", __FUNCTION__, pin->no);
+    LOGD("%s: %d", __FUNCTION__, pin->no);
 
-    ret = direction (pin, "in\0");
+    ret = direction(pin, "in\0");
 
     if (!ret)
         pin->direction = GPIO_IN;
@@ -269,11 +270,11 @@ int gpio_in (struct gpio_pin *pin) {
     return ret;
 }
 
-int gpio_set_value (struct gpio_pin *pin, gpio_value value) {
+int gpio_set_value(struct gpio_pin *pin, gpio_value value) {
     int ret = 0;
     char c;
 
-    LOGD("%s: %d = %d\n", __FUNCTION__, pin->no, value);
+    LOGD("%s: %d = %d", __FUNCTION__, pin->no, value);
 
     if (pin->direction != GPIO_OUT)
         return -EINVAL;
@@ -283,7 +284,7 @@ int gpio_set_value (struct gpio_pin *pin, gpio_value value) {
     }
 
     if (pin->fd == -1)
-        ret = open_value_fd (pin);
+        ret = open_value_fd(pin);
 
     if (ret)
         return ret;
@@ -301,7 +302,7 @@ int gpio_set_value (struct gpio_pin *pin, gpio_value value) {
             return -EINVAL;
     }
 
-    ret = write (pin->fd, &c, 1);
+    ret = write(pin->fd, &c, 1);
     if (ret != 1) {
         ret = -errno;
         close (pin->fd);
@@ -313,11 +314,11 @@ int gpio_set_value (struct gpio_pin *pin, gpio_value value) {
 }
 
 
-int gpio_get_value (struct gpio_pin *pin, gpio_value *value) {
+int gpio_get_value(struct gpio_pin *pin, gpio_value *value) {
     int ret = 0;
     char c;
 
-    LOGD("%s: %d\n", __FUNCTION__, pin->no);
+    LOGD("%s: %d", __FUNCTION__, pin->no);
 
     if (pin->valid != GPIO_VALID)
         return -ENOMEM;
@@ -328,7 +329,7 @@ int gpio_get_value (struct gpio_pin *pin, gpio_value *value) {
     if (ret)
         return ret;
 
-    ret = lseek (pin->fd, 0x00, SEEK_SET);
+    ret = lseek(pin->fd, 0x00, SEEK_SET);
     if (ret == -1) {
         ret = -errno;
         close (pin->fd);
@@ -336,7 +337,7 @@ int gpio_get_value (struct gpio_pin *pin, gpio_value *value) {
         return ret;
     }
 
-    ret = read (pin->fd, &c, 1);
+    ret = read(pin->fd, &c, 1);
     if (ret != 1) {
         ret = -errno;
         close (pin->fd);
@@ -362,10 +363,10 @@ int gpio_get_value (struct gpio_pin *pin, gpio_value *value) {
     return ret;
 }
 
-int gpio_open (struct gpio_pin *pin, unsigned int no) {
+int gpio_open(struct gpio_pin *pin, unsigned int no) {
     int ret = 0;
 
-    LOGD("%s: %d\n", __FUNCTION__, no);
+    LOGD("%s: %d", __FUNCTION__, no);
 
     pin->no = no;
     pin->fd = -1;
@@ -386,10 +387,10 @@ int gpio_open (struct gpio_pin *pin, unsigned int no) {
     return ret;
 }
 
-int gpio_open_dir (struct gpio_pin *pin, unsigned int no, gpio_direction dir) {
+int gpio_open_dir(struct gpio_pin *pin, unsigned int no, gpio_direction dir) {
     int ret = 0;
 
-    LOGD("%s: %d - %d\n", __FUNCTION__, no, dir);
+    LOGD("%s: %d - %d", __FUNCTION__, no, dir);
 
     ret = gpio_open(pin, no);
 
@@ -412,10 +413,10 @@ int gpio_open_dir (struct gpio_pin *pin, unsigned int no, gpio_direction dir) {
     return ret;
 }
 
-int gpio_close (struct gpio_pin *pin) {
+int gpio_close(struct gpio_pin *pin) {
     int ret = 0;
 
-    LOGD("%s: %d\n", __FUNCTION__, pin->no);
+    LOGD("%s: %d", __FUNCTION__, pin->no);
 
     if (pin->fd != -1) {
         close (pin->fd);
@@ -427,8 +428,8 @@ int gpio_close (struct gpio_pin *pin) {
     return ret;
 }
 
-int gpio_enable_irq (struct gpio_pin *pin, gpio_irq_mode m) {
-    LOGD("%s: %d\n", __FUNCTION__, pin->no);
+int gpio_enable_irq(struct gpio_pin *pin, gpio_irq_mode m) {
+    LOGD("%s: %d", __FUNCTION__, pin->no);
 
     if (pin->direction == GPIO_OUT)
         return -1;
@@ -439,12 +440,12 @@ int gpio_enable_irq (struct gpio_pin *pin, gpio_irq_mode m) {
     return set_irq(pin, m);
 }
 
-int gpio_get_fd (struct gpio_pin *pin) {
+int gpio_get_fd(struct gpio_pin *pin) {
     return pin->fd;
 }
 
 
-int gpio_irq_timed_wait (struct gpio_pin *pin, gpio_value *value, int timeout_ms) {
+int gpio_irq_timed_wait(struct gpio_pin *pin, gpio_value *value, int timeout_ms) {
     int ret = 0;
     struct pollfd irqdesc = {
         .events = POLLPRI | POLLERR ,
@@ -454,14 +455,14 @@ int gpio_irq_timed_wait (struct gpio_pin *pin, gpio_value *value, int timeout_ms
         return -ENOMEM;
 
     if (pin->fd == -1)
-        ret = open_value_fd (pin);
+        ret = open_value_fd(pin);
 
     if (ret)
         return ret;
 
     irqdesc.fd = pin->fd;
 
-    ret = poll (&irqdesc, 1, timeout_ms);
+    ret = poll(&irqdesc, 1, timeout_ms);
     if (ret == -1) {
         ret = -errno;
         close (pin->fd);
@@ -473,9 +474,9 @@ int gpio_irq_timed_wait (struct gpio_pin *pin, gpio_value *value, int timeout_ms
     if (ret == 0)
         return -1;
 
-    return gpio_get_value (pin, value);
+    return gpio_get_value(pin, value);
 }
 
-int gpio_irq_wait (struct gpio_pin *pin, gpio_value *value) {
-    return gpio_irq_timed_wait (pin, value, -1);
+int gpio_irq_wait(struct gpio_pin *pin, gpio_value *value) {
+    return gpio_irq_timed_wait(pin, value, -1);
 }
